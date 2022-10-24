34661403820c6f48139dd94bf6265981227bf01a
Aluno <aluno@etec.sp.gov.br>
2022-10-24 20:43:07 -0300

    Aula-4 mais coisas

---
+++ Exec-2/src/index.js
@@ -0,0 +1,35 @@
+function escolheOperacao(opcao, p1, p2, p3, p4){
+    switch(opcao){
+        case "pitagoras":
+            return calculaPitagoras(p1, p2, p3);
+            break;
+        case "regra":
+            return calculaRegra(p1, p2, p3, p4);
+            break;
+        case "calculadora":
+            return calculaNumeros(p1, p2, p3);
+            break;
+        default:
+            return "fora do escopo de funções."
+    }
+}
+function calculaPitagoras(){
+    let p1 = document.getElementById(p1).Value;
+    let p2 = document.getElementById(p2).Value;
+    let p3 = document.getElementById(p3).Value;
+    
+}
+
+
+
+
+
+
+
+function calculaRegra
+
+
+
+
+function calculaNumeros
+
