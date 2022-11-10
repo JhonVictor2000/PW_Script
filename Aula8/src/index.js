85b239da5a39857f4dddf3be41ee342a6bc3031a
Aluno <aluno@etec.sp.gov.br>
2022-11-09 22:42:56 -0300

    Aula 8 Vetores

--- Aula8/src/index.js
+++ Aula8/src/index.js
@@ -1,18 +1,20 @@
-const numeros = new Array(15, 13, 16, 22, 35, 87, 64, 19)
-const nomes = new Array("Victor", "Lucas", "Rafael")
-const data = new Date("2022-11-09")
-function dataComNumeros(){
-    let juntos = numeros.concat(nomes)
-    console.log(numeros.sort(function(a, b){return a-b}))
-    console.log(juntos)
-    juntos.splice(3, 0, "Cachorro", "Gato")
-    console.log(juntos)
-    console.log(data)
-    console.log(data.getFullYear())
-}
-
-let html =""
-numeros.forEach(function (valor, indice, array){
-    html += valor + " posicao |" + indice +" | "
+const saldoPessoas = new Array(
+    {
+        nome: "Victor",
+        saldo: 2030
+    },
+    {
+        nome: "Lucas",
+        saldo: 6480
+    },
+    {
+        nome: "Olga",
+        saldo: 10560
+    }
+)
+saldoPessoas.push({nome: "Guastavo",saldo:5630})
+saldoPessoas.sort(function(a,b){return a.saldo - b.saldo});
+saldoPessoas.forEach(function (valor, indice, vetor){
+    texto += "Posicao: "+indice+" Nome: "+valor.nome+" | Salario: "+valor.saldo+"."
 })
-console.log(html)
+console.log(texto)
